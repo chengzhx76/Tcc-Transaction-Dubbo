0035350e65afd330aea6afb4a319c058a76d1de4
chengzhx76 <chengzhx76@qq.com>
2017-05-19 13:53:19 +0800

    TCC

---
+++ sql/tcc.sql
@@ -0,0 +1,112 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost-1
+Source Server Version : 50626
+Source Host           : localhost:3306
+Source Database       : tcc
+
+Target Server Type    : MYSQL
+Target Server Version : 50626
+File Encoding         : 65001
+
+Date: 2017-05-19 13:51:08
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tcc_transaction_cap
+-- ----------------------------
+DROP TABLE IF EXISTS `tcc_transaction_cap`;
+CREATE TABLE `tcc_transaction_cap` (
+  `TRANSACTION_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DOMAIN` varchar(100) DEFAULT NULL,
+  `GLOBAL_TX_ID` varbinary(32) NOT NULL,
+  `BRANCH_QUALIFIER` varbinary(32) NOT NULL,
+  `CONTENT` varbinary(8000) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  `TRANSACTION_TYPE` int(11) DEFAULT NULL,
+  `RETRIED_COUNT` int(11) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `LAST_UPDATE_TIME` datetime DEFAULT NULL,
+  `VERSION` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TRANSACTION_ID`),
+  UNIQUE KEY `UX_TX_BQ` (`GLOBAL_TX_ID`,`BRANCH_QUALIFIER`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tcc_transaction_cap
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tcc_transaction_ord
+-- ----------------------------
+DROP TABLE IF EXISTS `tcc_transaction_ord`;
+CREATE TABLE `tcc_transaction_ord` (
+  `TRANSACTION_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DOMAIN` varchar(100) DEFAULT NULL,
+  `GLOBAL_TX_ID` varbinary(32) NOT NULL,
+  `BRANCH_QUALIFIER` varbinary(32) NOT NULL,
+  `CONTENT` varbinary(8000) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  `TRANSACTION_TYPE` int(11) DEFAULT NULL,
+  `RETRIED_COUNT` int(11) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `LAST_UPDATE_TIME` datetime DEFAULT NULL,
+  `VERSION` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TRANSACTION_ID`),
+  UNIQUE KEY `UX_TX_BQ` (`GLOBAL_TX_ID`,`BRANCH_QUALIFIER`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tcc_transaction_ord
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tcc_transaction_red
+-- ----------------------------
+DROP TABLE IF EXISTS `tcc_transaction_red`;
+CREATE TABLE `tcc_transaction_red` (
+  `TRANSACTION_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DOMAIN` varchar(100) DEFAULT NULL,
+  `GLOBAL_TX_ID` varbinary(32) NOT NULL,
+  `BRANCH_QUALIFIER` varbinary(32) NOT NULL,
+  `CONTENT` varbinary(8000) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  `TRANSACTION_TYPE` int(11) DEFAULT NULL,
+  `RETRIED_COUNT` int(11) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `LAST_UPDATE_TIME` datetime DEFAULT NULL,
+  `VERSION` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TRANSACTION_ID`),
+  UNIQUE KEY `UX_TX_BQ` (`GLOBAL_TX_ID`,`BRANCH_QUALIFIER`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tcc_transaction_red
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tcc_transaction_ut
+-- ----------------------------
+DROP TABLE IF EXISTS `tcc_transaction_ut`;
+CREATE TABLE `tcc_transaction_ut` (
+  `TRANSACTION_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DOMAIN` varchar(100) DEFAULT NULL,
+  `GLOBAL_TX_ID` varbinary(32) NOT NULL,
+  `BRANCH_QUALIFIER` varbinary(32) NOT NULL,
+  `CONTENT` varbinary(8000) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  `TRANSACTION_TYPE` int(11) DEFAULT NULL,
+  `RETRIED_COUNT` int(11) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `LAST_UPDATE_TIME` datetime DEFAULT NULL,
+  `VERSION` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TRANSACTION_ID`),
+  UNIQUE KEY `UX_TX_BQ` (`GLOBAL_TX_ID`,`BRANCH_QUALIFIER`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tcc_transaction_ut
+-- ----------------------------
